7c5afc06e7186a321a1bc773126eec03314ad7b1
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-12-09 16:42:21 -0600

    almost done but the grid isn't showing :(

--- C++Project/Name.cpp
+++ C++Project/Name.cpp
@@ -20,6 +20,25 @@
 //	}
 //}
 
+void printArray(int j)
+{
+	int numbers[1000];
+
+	for (int i = 0; i < j; i++)
+	{
+		std::cout << numbers[i] << std::endl;
+	}
+}
+
+void choicePrintArray(int arr[], int length)
+{
+	for (int i = 0; i < length; i++)
+		std::cout << arr[i] << std::endl;
+
+	system("pause");
+
+}
+
 void GuessingGame()
 {
 	int numbers[10];
@@ -90,5 +109,24 @@ int main()
 	system("pause");
 	system("cls");
 
-	return 1;
+	int j = 0;
+
+	std::cout << "what number do you wish for the program to count to? :";
+	std::cin >> j;
+
+	printArray(j);
+
+	system("pause");
+	system("cls");
+
+	int ii = 0;
+	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+
+	std::cout << "what number do you wish for the program to count to? :";
+	std::cin >> ii;
+
+	choicePrintArray(numbers, ii);
+
+	system("pause");
+	system("cls");
 }
--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,43 +1,144 @@
 #include <iostream>
 
-void printArray(int j)
+
+
+
+void TicTacGrid(char arr[])
 {
-	int numbers[1000];
-
-	for (int i = 0; i < j; i++)
-	{
-		std::cout << numbers[i] << std::endl;
-	}
-}
-
-void choicePrintArray(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		std::cout << arr[i] << std::endl;
-
-	system("pause");
-
+	std::cout << arr[0] << arr[1] << arr[2] << std::endl;
+	std::cout << arr[3] << arr[4] << arr[5] << std::endl;
+	std::cout << arr[6] << arr[7] << arr[8] << std::endl;
 }
 
 int main()
 {
-	int j = 0;
-
-	std::cout << "what number do you wish for the program to count to? :";
-	std::cin >> j;
-
-	printArray(j);
+	std::cout << "Welcome to Tic Tac Toe!" << std::endl;
+	std::cout << "Player one you are X!" << std::endl;
+	std::cout << "Player two you are O!" << std::endl;
 
 	system("pause");
 	system("cls");
 
-	int ii = 0;
-	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+	bool gameOver = false;
+	char grid[9];
 
-	std::cout << "what number do you wish for the program to count to? :";
-	std::cin >> ii;
+	char xCoin = 'X';
+	char yCoin = 'O';
 
-	choicePrintArray(numbers, ii);
+	std::cout << "Press the numbers corisponding to the placements!" << std::endl;
+
+	std::cout << "1" << " 2 " << "3" << std::endl;
+	std::cout << "4" << " 5 " << "6" << std::endl;
+	std::cout << "7" << " 8 " << "9" << std::endl;
+
+	system("pause");
+	system("cls");
+
+	while (gameOver == false)
+	{
+		int player1 = 0;
+		int player2 = 0;
+
+		//player 1
+		std::cin >> player1;
+
+		//this will get player 1's choice and implement X in the place they chose
+		if (player1 = 1)
+		{
+			grid[0] = xCoin;
+		}
+		else if (player1 = 2)
+		{
+			grid[1] = xCoin;
+		}
+		else if (player1 = 3)
+		{
+			grid[2] = xCoin;
+		}
+		else if (player1 = 4)
+		{
+			grid[3] = xCoin;
+		}
+		else if (player1 = 5)
+		{
+			grid[4] = xCoin;
+		}
+		else if (player1 = 6)
+		{
+			grid[5] = xCoin;
+		}
+		else if (player1 = 7)
+		{
+			grid[6] = xCoin;
+		}
+		else if (player1 = 8)
+		{
+			grid[7] = xCoin;
+		}
+		else if (player1 = 9)
+		{
+			grid[8] = xCoin;
+		}
+		else
+		{
+			std::cout << "Error. Turn Skipped" << std::endl;
+		}
+
+		TicTacGrid(grid);
+
+		system("pause");
+		system("cls");
+
+		//player 2
+		std::cin >> player2;
+
+		//this will get player 2's choice and implement O in the place they chose
+		if (player2 = 1)
+		{
+			grid[0] = yCoin;
+		}
+		else if (player2 = 2)
+		{
+			grid[1] = yCoin;
+		}
+		else if (player2 = 3)
+		{
+			grid[2] = yCoin;
+		}
+		else if (player2 = 4)
+		{
+			grid[3] = yCoin;
+		}
+		else if (player2 = 5)
+		{
+			grid[4] = yCoin;
+		}
+		else if (player2 = 6)
+		{
+			grid[5] = yCoin;
+		}
+		else if (player2 = 7)
+		{
+			grid[6] = yCoin;
+		}
+		else if (player2 = 8)
+		{
+			grid[7] = yCoin;
+		}
+		else if (player2 = 9)
+		{
+			grid[8] = yCoin;
+		}
+		else
+		{
+			std::cout << "Error. Turn Skipped" << std::endl;
+		}
+
+		TicTacGrid(grid);
+
+		system("pause");
+		system("cls");
+	}
 
 	system("pause");
 	system("cls");
