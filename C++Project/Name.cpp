92bf4d7cebaf89bacc714dc87eeed514fb009daf
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-12-09 11:10:32 -0600

    rearranged

--- C++Project/Name.cpp
+++ C++Project/Name.cpp
@@ -20,25 +20,8 @@
 //	}
 //}
 
-
-int main()
+void GuessingGame()
 {
-	int j = 0;
-	int number = 0;
-
-	//Similar to: Console.Write();
-	/*std::cout << "please type random number!: ";*/
-
-	//gets player input as a value
-	/*std::cin >> number;
-
-	FizzBuzz(number, j);*/
-
-	//waitd for player input
-	system("pause");
-	//clears screen
-	system("cls");
-
 	int numbers[10];
 	int min = 1;
 	int max = 10;
@@ -81,6 +64,28 @@ int main()
 			max = guess - 1;
 		}
 	}
+}
+
+
+int main()
+{
+	int j = 0;
+	int number = 0;
+
+	//Similar to: Console.Write();
+	/*std::cout << "please type random number!: ";*/
+
+	//gets player input as a value
+	/*std::cin >> number;
+
+	FizzBuzz(number, j);*/
+
+	//waitd for player input
+	system("pause");
+	//clears screen
+	system("cls");
+
+	GuessingGame();
 
 	system("pause");
 	system("cls");
