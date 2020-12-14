10a2301ec69fddea55627ed44ce3e74a6e852ed9
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-12-14 12:13:53 -0600

    new lesson, teaching scope in c++

--- C++Project/Name.cpp
+++ C++Project/Name.cpp
@@ -19,114 +19,117 @@
 //		j++;
 //	}
 //}
-
-void printArray(int j)
-{
-	int numbers[1000];
-
-	for (int i = 0; i < j; i++)
-	{
-		std::cout << numbers[i] << std::endl;
-	}
-}
-
-void choicePrintArray(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		std::cout << arr[i] << std::endl;
-
-	system("pause");
-
-}
-
-void GuessingGame()
-{
-	int numbers[10];
-	int min = 1;
-	int max = 10;
-	int passed = 0;
-
-	std::cout << "Welcome Players! This is the guessing game!" << std::endl;
-	std::cout << "Pick a number between 1 and 10!" << std::endl;
-
-	while (min <= max)
-	{
-		int a = 0;
-		bool goal = false;
-		int guess = (min + max) / 2;
-
-		std::cout << guess << ": Is this your number?:" << std::endl;
-		std::cout << "[1] Yes" << std::endl;
-		std::cout << "[2] Lower" << std::endl;
-		std::cout << "[3] Higher" << std::endl;
-		std::cin >> a;
-
-		if (a == 1)
-		{
-			std::cout << "Yes! I got it!" << std::endl;
-			break;
-		}
-		else if (a == 3)
-		{
-			if (min == max)
-			{
-				std::cout << "You changed your number! Cheater!" << std::endl;
-			}
-			min = guess + 1;
-		}
-		else if (a == 2)
-		{
-			if (min == max)
-			{
-				std::cout << "You changed your number! Cheater!" << std::endl;
-			}
-			max = guess - 1;
-		}
-	}
-}
+//
+//void printArray(int j)
+//{
+//	int numbers[1000];
+//
+//	for (int i = 0; i < j; i++)
+//	{
+//		std::cout << numbers[i] << std::endl;
+//	}
+//}
+//
+//void choicePrintArray(int arr[], int length)
+//{
+//	for (int i = 0; i < length; i++)
+//		std::cout << arr[i] << std::endl;
+//
+//	system("pause");
+//
+//}
+//
+//void GuessingGame()
+//{
+//	int numbers[10];
+//	int min = 1;
+//	int max = 10;
+//	int passed = 0;
+//
+//	std::cout << "Welcome Players! This is the guessing game!" << std::endl;
+//	std::cout << "Pick a number between 1 and 10!" << std::endl;
+//
+//	while (min <= max)
+//	{
+//		int a = 0;
+//		int goal = 0;
+//		int guess = (min + max) / 2;
+//		guess = numbers[guess];
+//		int answer = 0;
+//
+//		std::cout << guess << ": Is this your number?1. higher 2.lower 3. You got it![1/2/3]:";
+//		std::cin >> answer;
+//		if (answer = 1)
+//		{
+//
+//		}
+//
+//		if (numbers[guess] == goal)
+//		{
+//			goal = true;
+//			std::cout << "Yes! I firgured out the number!";
+//		}
+//		else if (a == 3)
+//		{
+//			if (min == max)
+//			{
+//				std::cout << "You changed your number! Cheater!" << std::endl;
+//			}
+//			min = guess + 1;
+//		}
+//		else if (a == 2)
+//		{
+//			if (min == max)
+//			{
+//				std::cout << "You changed your number! Cheater!" << std::endl;
+//			}
+//			max = guess - 1;
+//		}
+//	}
+//}
 
 
 int main()
 {
-	int j = 0;
-	int number = 0;
+	//int j = 0;
+	//int number = 0;
 
-	//Similar to: Console.Write();
-	/*std::cout << "please type random number!: ";*/
+	////Similar to: Console.Write();
+	///*std::cout << "please type random number!: ";*/
 
-	//gets player input as a value
-	/*std::cin >> number;
+	////gets player input as a value
+	///*std::cin >> number;
 
-	FizzBuzz(number, j);*/
+	//FizzBuzz(number, j);*/
 
-	//waitd for player input
-	system("pause");
-	//clears screen
-	system("cls");
+	////waitd for player input
+	//system("pause");
+	////clears screen
+	//system("cls");
 
-	GuessingGame();
+	//GuessingGame();
 
-	system("pause");
-	system("cls");
+	//system("pause");
+	//system("cls");
 
-	int j = 0;
+	//j = 0;
 
-	std::cout << "what number do you wish for the program to count to? :";
-	std::cin >> j;
+	//std::cout << "what number do you wish for the program to count to? :";
+	//std::cin >> j;
 
-	printArray(j);
+	//printArray(j);
 
-	system("pause");
-	system("cls");
+	//system("pause");
+	//system("cls");
 
-	int ii = 0;
-	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+	//int ii = 0;
+	//int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
 
-	std::cout << "what number do you wish for the program to count to? :";
-	std::cin >> ii;
+	//std::cout << "what number do you wish for the program to count to? :";
+	//std::cin >> ii;
 
-	choicePrintArray(numbers, ii);
+	//choicePrintArray(numbers, ii);
 
-	system("pause");
-	system("cls");
+	//system("pause");
+	//system("cls");
 }
